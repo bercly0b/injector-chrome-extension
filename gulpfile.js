769052c6a18530bb6f737583c08a815150caaad5
bercly0b <vvworkmail@yandex.ru>
2019-01-28 20:14:24 +0300

    build: gulp task for all + min all

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ const gulp = require('gulp')
 const browserify = require('browserify')
 const source = require('vinyl-source-stream')
 const buffer = require('vinyl-buffer')
+const uglify = require('gulp-uglify-es').default
 const sass = require('gulp-sass')
+const htmlmin = require('gulp-htmlmin')
+const cleanCSS = require('gulp-clean-css')
 
 const path = {
   bg: 'src/background/index.js',
@@ -10,12 +13,32 @@ const path = {
   popup: 'src/popup/index.js',
   allScript: 'src/**/*.js',
   scss: 'src/**/*.scss',
+  html: 'src/popup.html',
+  manifest: 'src/manifest.json',
+  icons: 'src/icons/*.png',
   build: 'bundle',
 }
+
+gulp.task('html', () => {
+  return gulp.src(path.html)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(path.build))
+})
+
+gulp.task('icons', () => {
+  return gulp.src(path.icons)
+    .pipe(gulp.dest(`${path.build}/icons`))
+})
+
+gulp.task('manifest', () => {
+  return gulp.src(path.manifest)
+    .pipe(gulp.dest(path.build))
+})
 
 gulp.task('scss', () => {
   return gulp.src(path.scss)
     .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
     .pipe(gulp.dest(path.build))
 })
 
@@ -24,6 +47,7 @@ gulp.task('buildBg', () => {
     .bundle()
     .pipe(source('background.js'))
     .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest(path.build))
 })
 
@@ -32,6 +56,7 @@ gulp.task('buildContent', () => {
     .bundle()
     .pipe(source('content.js'))
     .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest(path.build))
 })
 
@@ -40,14 +65,18 @@ gulp.task('buildPopup', () => {
     .bundle()
     .pipe(source('popup.js'))
     .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest(path.build))
 })
 
 gulp.task('watch', () => {
   gulp.watch(path.scss, gulp.series('scss'))
+  gulp.watch(path.html, gulp.series('html'))
+  gulp.watch(path.icons, gulp.series('icons'))
+  gulp.watch(path.manifest, gulp.series('manifest'))
+  gulp.watch(path.scss, gulp.series('scss'))
   gulp.watch(path.allScript, gulp.parallel('buildBg', 'buildContent', 'buildPopup'))
 })
 
-gulp.task('build', gulp.parallel('scss', 'buildBg', 'buildContent', 'buildPopup'))
-
+gulp.task('build', gulp.parallel('scss', 'buildBg', 'buildContent', 'buildPopup', 'manifest', 'html', 'icons'))
 gulp.task('default', gulp.parallel('watch'))
